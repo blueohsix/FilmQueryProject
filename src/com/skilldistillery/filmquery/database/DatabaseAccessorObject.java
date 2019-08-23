5cf24d66bf319e801440eb2f6a7ac1400f19c737
Casey Asher <casey.e.asher@outlook.com>
2019-08-23 16:36:09 -0600

    first commit

---
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -0,0 +1,85 @@
+package com.skilldistillery.filmquery.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.skilldistillery.filmquery.entities.Actor;
+import com.skilldistillery.filmquery.entities.Film;
+
+//TEMPLATE FOR CONNECTING TO A DATABASE
+public class DatabaseAccessorObject implements DatabaseAccessor {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
+	private final String userName = "student";
+	private final String password = "student";
+
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.err.println(e);
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Film findFilmById(int filmId) {
+		Film film = null;
+
+		try {
+			Connection conn = DriverManager.getConnection(URL, userName, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public Actor findActorById(int actorId) throws SQLException {
+		Actor actor = null;
+		Connection conn = DriverManager.getConnection(URL, userName, password);
+		String sql = "SELECT id, first_name, last_name FROM actor WHERE id = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, actorId);
+		ResultSet actorResult = stmt.executeQuery();
+		while (actorResult.next()) {
+			actor = new Actor(actorId, sql, sql); // Create the object
+			// Here is our mapping of query columns to our object fields:
+			actor.setId(actorResult.getInt("id"));
+			actor.setFirstName(actorResult.getString("first_name"));
+			actor.setLastName(actorResult.getString("last_name"));
+		}
+		actorResult.close();
+		stmt.close();
+		conn.close();
+		return actor;
+	}
+
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) throws SQLException {
+		Actor actor = null;
+		Connection conn = DriverManager.getConnection(URL, userName, password);
+		String sql = "SELECT id, first_name, last_name FROM actor WHERE film.id = ?"; //needs JOIN
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, filmId);
+		ResultSet actorResult = stmt.executeQuery();
+		while (actorResult.next()) {
+			actor = new Actor(filmId, sql, sql); // Create the object
+			// Here is our mapping of query columns to our object fields:
+			actor.setId(actorResult.getInt("id"));
+			actor.setFirstName(actorResult.getString("first_name"));
+			actor.setLastName(actorResult.getString("last_name"));
+		}
+		actorResult.close();
+		stmt.close();
+		conn.close();
+		return (List<Actor>) actor;
+	}
+
+}
